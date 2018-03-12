727e76a4b4d5fa6ca774ba010e6b70e12b625934
CM Student <cmstudent@UHWO-ACM-C-05.local>
2018-03-12 13:34:01 -1000

    Finished About Me and BucketList

--- Hackwich3/ViewController.swift
+++ Hackwich3/ViewController.swift
@@ -12,13 +12,41 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var firstLabel: UILabel!
     
+    @IBOutlet weak var myName: UILabel!
+    
+    @IBOutlet weak var myMajor: UILabel!
+    
+    @IBOutlet weak var iMajored: UILabel!
+    
+    @IBOutlet weak var aboutMe: UILabel!
+    
+    @IBOutlet weak var paragraphAbout: UITextView!
+    
+    @IBOutlet weak var myProfilePic: UIImageView!
+    
+    @IBOutlet weak var myLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //set firstLabel to the string "about"
         
-self.firstLabel.text = "about"
+        self.firstLabel.text = "My Name is:"
     
+        self.myName.text = "David"
+        
+        self.myMajor.text = "My Major is:"
+        
+        self.iMajored.text = "I majoring in Applied Science Creative Media"
+        
+        iMajored.font = UIFont.systemFont(ofSize: 12.0)
+        
+        self.aboutMe.text = "About Me"
+        
+        self.paragraphAbout.text = "I was born and raised on the island of Oahu, Hawaii. I do community service along with my church where my whole church will go to cerent places and clean it up and make all brand new."
+        
+        myProfilePic.image = UIImage(named: "IMG_2337")
+        myLogo.image = UIImage(named: "SAO")
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Hackwich3/thirdViewController.swift
@@ -0,0 +1,52 @@
+//
+//  thirdViewController.swift
+//  Hackwich3
+//
+//  Created by CM Student on 3/12/18.
+//  Copyright © 2018 CM Student. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController, UITableViewDataSource,UITableViewDelegate  {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    var myBucketListArray =  ["I would want to visit Japan", "I would like to meet and celebrity", "I want to check out a national mounument", "I would like to take a tour of Funimation", "I would like to visit China"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return myBucketListArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //set up cell to display items in studentArray
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuselIdentifer")!
+        let text = myBucketListArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
